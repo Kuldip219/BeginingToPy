41004d12483d499f238636e7286f382d60096287
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-01 19:41:33 +0600

    Add arithmetic operations to project1.py
    Extended Chapter2/project1.py to include division, addition, and subtraction operations in addition to multiplication.

--- Chapter2/project1.py
+++ Chapter2/project1.py
@@ -3,3 +3,6 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 print("The multiplied result is:", a * b)
+print("The divided result is:", a / b)
+print("The sum result is:", a + b)
+print("The subtracted result is:", a - b)
