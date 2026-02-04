a2418d42f12694f93334987c315e2b53e41bac97
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-04 21:13:11 +0600

    Add driving eligibility script
    Add Chapter6/06_project_2.py: a simple CLI that prompts the user for their age and prints whether they are old enough to drive.

---
+++ Chapter6/06_project_2.py
@@ -0,0 +1,14 @@
+
+print("Welcome to this project! \nAre you old enough to drive? Let's find out!")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are old enough to drive!")
+
+elif age <= 0:
+    print("Please enter a valid age.")
+
+else:
+    print("You are not old enough to drive yet.")
+
+# End of the code
