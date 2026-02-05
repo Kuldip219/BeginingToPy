5e3414553cd14bd131d068ce6e3f4d5e1335559f
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-05 09:52:51 +0600

    Add simple spam keyword checker
    Add Chapter6/06_project_3.py: an interactive script that prompts the user and checks the input for any of three spam.

---
+++ Chapter6/06_project_3.py
@@ -0,0 +1,12 @@
+s1 = "eats rice"
+s2 = "playing football"
+s3 = "riding a bike"
+
+massage = input("Enter your massage:")
+
+if s1 in massage or s2 in massage or s3 in massage:
+    print("your massage includes a spam")
+else:
+    print("your massage is clean")
+
+# End of the code
