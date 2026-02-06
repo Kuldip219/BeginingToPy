1777f52cb9e3876468aea2302c49557e6c4b168a
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-06 20:23:05 +0600

    Add while-loop example printing 1-20
    Added an equivalent while-loop implementation with accompanying comments.

--- Chapter7/07_project_1.py
+++ Chapter7/07_project_1.py
@@ -1,3 +1,13 @@
 for n in range(1,21):
     print(n)
+
 # This code prints numbers from 1 to 20 using a for loop.
+
+
+a = 1
+
+while a <= 20:
+    print(a)
+    a += 1
+
+# This code prints numbers from 1 to 20 using a while loop.
