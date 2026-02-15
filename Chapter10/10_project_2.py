2a42a3bad06812650b2f427f513c85b871203447
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-15 19:52:25 +0600

    Update 10_project_2
    Added another program that extracts the squareroot of the given number.

--- Chapter10/10_project_2.py
+++ Chapter10/10_project_2.py
@@ -8,6 +8,12 @@ class calculator:
     def cube(self):
         print(f"The cube of {self.num} is {self.num ** 3}")
 
+    def square_root(self):
+        print(f"The square root of {self.num} is {self.num ** 0.5}")
+
 a = calculator(int(input("Enter a number: ")))
 a.square()
 a.cube()
+a.square_root()
+
+# End of the code snippet.
