59a2d80814ef568536029ab47d0d1138902fc1e0
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-17 20:06:30 +0600

    Update 11_project_1.py

--- Chapter11/11_project_1.py
+++ Chapter11/11_project_1.py
@@ -8,4 +8,10 @@ class cat(pets):
     def __init__(self):
         print("The cat says.... \n Meowwwwwwwww")
 
+class dog(pets):
+    def __init__(self):
+        print("      and \n The dog says.... \n Woof Woof!")
+
+
 a = cat()
+b = dog()
