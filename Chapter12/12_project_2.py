0c08b771b3d5984bd832f2b63ad7643f392e4b97
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-19 20:47:08 +0600

    Updated 12_project_2.py

--- Chapter12/12_project_2.py
+++ Chapter12/12_project_2.py
@@ -5,6 +5,8 @@ mylist = [1, 5, 3, 10, 7, 4]
 for index, item in enumerate(mylist):
     print(f"The item at index {index} is {item}")
 
+print("\nHere is the list with squared numbers....")
+
 # The code below will create a new list that contains the squares of the items in the original list.
 
 squaredlist = [item ** 2 for item in mylist]
