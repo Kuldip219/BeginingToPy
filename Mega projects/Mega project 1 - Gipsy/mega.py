0952781c7357a0a6f0e1b98831e31e21da2e7399
Siddartho Sarker Bipro <138643317+Kuldip219@users.noreply.github.com>
2026-02-26 19:06:21 +0600

    updated mega.py
    Added some small adjustments.

--- Mega projects/Mega project 1 - Gipsy/mega.py
+++ Mega projects/Mega project 1 - Gipsy/mega.py
@@ -9,17 +9,23 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
 if __name__ == "__main__":
     speak("Hello! I am Gipsy, your personal assistant.")
+    print("OK")
     while True:
         r = sr.Recognizer()
         try:
             with sr.Microphone() as source:
+                print("Adjusting...")
+                recognizer.adjust_for_ambient_noise(source, duration=1)
+
                 print("Listening...")
-                audio = r.listen(source, timeout=2, phrase_time_limit=1)
-                command = r.recognize_google(audio).lower()
-            if(command == "Gipsy"):
-                speak("Yes, how can I assist you?")
-        
+                audio = r.listen(source)
+
+                print("Recognizing...")
+                word = r.recognize_google(audio)
+                print(word)
+
         except Exception as e:
-            print("Error; {0}".format(e))
+            print("Error; {0}".format(e))        
